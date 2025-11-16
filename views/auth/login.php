965cfd29bc7ce106ae310b59dd9c873054ae1e53
ymarte20023 <yamartealondra@gmail.com>
2025-11-16 19:04:54 -0400

    Login de Incio de seccion

---
+++ views/auth/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Login</title>
+</head>
+
+<body>
+    <!--1.1 Formulario de Inicio de seccion-->
+   <div class="form">
+        <h2>Inicio de Seccion</h2>
+        <form>
+            <label>Correo electronico</label>
+            <input type="email" id="email" name="email">
+
+            <label>Contraseña</label>
+            <input type="password" id="pwd" name="pwd" required>
+            
+            <button type="submit">Inicio de seccion</button>
+        </form>
+    </div>
+</body>
+</html>
